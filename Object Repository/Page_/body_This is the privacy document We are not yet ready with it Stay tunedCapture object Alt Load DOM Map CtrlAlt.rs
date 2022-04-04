bb92c1adccba0cd8bba58f959caeca2b4cdde769
RubenYsabel <RubenYsabel@DESKTOP-S020PT8>
2022-04-04 13:12:06 -0400

    Actualizacion evaluacion

---
+++ Object Repository/Page_/body_This is the privacy document We are not yet ready with it Stay tunedCapture object Alt Load DOM Map CtrlAlt.rs
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>body_This is the privacy document We are not yet ready with it Stay tunedCapture object Alt Load DOM Map CtrlAlt</name>
+   <tag></tag>
+   <elementGuidId>ea241fef-ad13-4f50-8520-09d91f734c27</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//*/text()[normalize-space(.)='This is the privacy document. We are not yet ready with it. Stay tuned!']/parent::*</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>body</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>This is the privacy document. We are not yet ready with it. Stay tuned!Capture object: Alt ` ● Load DOM Map: Ctrl Alt ` </value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='This is the privacy document. We are not yet ready with it. Stay tuned!']/parent::*</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//body</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/Evaluacion.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Evaluacion</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>727d44af-2e77-4134-9ed3-65c4affaf791</testSuiteGuid>
+   <testCaseLink>
+      <guid>fc43f53c-1146-40c0-ab04-469af5fb3f8d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Validaciones</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2fa74c7c-4581-45cd-9a5c-e5f3dff68879</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Valdacion 2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
